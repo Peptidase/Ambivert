27d5258a829d886337813ccbddee9bf55fee7f79
Peptidase <ArifMeighan@hotmail.co.uk>
2021-04-22 18:50:23 +0100

    removing from local drive

--- script.js
+++ script.js
@@ -1,71 +1,50 @@
-//Made by arif from youtube tutorial
+google.charts.load('current', {
+    'packages':['geochart'],
+    // Note: you will need to get a mapsApiKey for your project.
+    // See: https://developers.google.com/chart/interactive/docs/basic_load_libs#load-settings
+    'mapsApiKey': 'AIzaSyD-9tSrke72PouQMnMX-a7eZSW0jkFMBWY'
+  });
+  google.charts.setOnLoadCallback(drawRegionsMap);
 
-const canvas = document.getElementById("canvas");
-console.log("Running!")
-const ctx = canvas.getContext('2d');
+  function drawRegionsMap() {
+    var data = google.visualization.arrayToDataTable([
+      ['Country', 'Popularity'],
+      ['Germany', 200],
+      ['United States', 300],
+      ['Brazil', 400],
+      ['Canada', 500],
+      ['France', 600],
+      ['RU', 700]
+    ]);
 
-ctx.canvas.width= window.innerWidth;
-ctx.canvas.height= window.innerHeight;
+    var options = {};
 
-let particleArray;
+    var chart = new google.visualization.GeoChart(document.getElementById('TradingRegions'));
+
+    chart.draw(data, options);
+  }
 
 
-//Constructor
-function Particle(x,y,directionX,directionY,size,color){
-    this.x = x;
-    this.y = y;
-    this.directionX = directionX;
-    this.directionY = directionY;
-    this.size = size;
-    this.color = color;
-}
-//drawer
-
-Particle.prototype.draw = function(){
-    ctx.beginPath();
-    ctx.arc(this.x,this.y,this.size,0,Math.PI*2,false);
-    ctx.fillStyle = this.color;
-    ctx.fill();
-}
-//Updter
-Particle.prototype.update = function(){
-    if(this.x + this.size > canvas.width || this.x - this.size < 0){
-        this.directionX =-this.directionX;
+var ctx = document.getElementById('myChart').getContext('2d');
+var myChart = new Chart(ctx, {
+    type: 'line',
+    data: {
+        labels: ['Day 1', 'Day 2', 'Day 3', 'Day 4', 'Day 5', 'Day 6'],
+        datasets: [{
+            label: 'Price Of Portfolio',
+            data: [12, 19, 3, 5, 2, 3],
+            borderColor: ['rgba(155,155,155)'
+            ],
+            borderWidth: 2
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    
+                }
+            }]
+        }
     }
-    if(this.y + this.size > canvas.height || this.y - this.directionY < 0){
-        this.directionY = -this.directionY;
-    }
-    this.x += this.directionX;
-    this.y = this.directionY;
-    this.draw();
-}
-
-//create particle array
-function init(){
-    particleArray = [];
-    for(let i=0; i<10; i++){
-        let size = Math.random() *20;
-        let x = Math.random * (innerWidth - size * 2);
-        let y = Math.random * (innerHeight- size * 2)
-        let directionX = (Math.random() * .4) -2;
-        let directionY = (Math.random() * .4) -2;
-        let color = 'rgb(255,255,255)';
-        particleArray.push(new Particle(x,y,directionX,directionY,size,color));
-        console.log("Particle!" + i + "Made");
-
-    }
-}
-
-function animate(){
-    requestAnimationFrame(animate);
-    ctx.clearRect(0,0, innerWidth,innerHeight);
-
-    for (let i=0; i<particleArray.length;i++){
-        particleArray[i].update();
-        
-       
-    }
-}
-
-init();
-animate();
+});
